b634d371793653b07ececbd740847570af41d61f
Srujan Vajram <55774184+vajramsrujan@users.noreply.github.com>
2025-05-04 12:26:53 -0400

    Leaderboard update
    Added leaderboard. Removed redundant code

--- app.py
+++ app.py
@@ -4,19 +4,16 @@ import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    logs = requests.get("http://localhost:9000/logs").json()
-    return render_template("index.html", logs=logs)
-
-@app.route('/whiteboard')
 def whiteboard():
     return render_template("whiteboard.html")
+
+
 @app.route('/chains')
 def get_chains():
     chains = requests.get("http://localhost:9000/chains").json()
     return jsonify(chains)
+
 
 if __name__ == "__main__":
     app.run(port=8000)
